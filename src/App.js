7a04b69ef06483121df27cd488797db1b48c2bbd
kowais915 <kowais915@gmail.com>
2023-07-11 06:48:36 +0500

    :adhesive_bandage: Set the login page as the landing page

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
               }
             />
 
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
 
             <Route path="/signup" element={<Signup />} />
           </Routes>
